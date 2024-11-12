ffd26363fa23aa4d12e35ddf23afd2cc2841aa82
Daniel Chmielewski <contact@dcsoftware.xyz>
2024-11-12 15:53:44 +0000

    Remove unused imports

--- programs/tvp/src/lib.rs
+++ programs/tvp/src/lib.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
 declare_id!("6srUu57dvG9XpgcfxJGp3yR2Pg6dDZzc2F4Aydwns5gv");
